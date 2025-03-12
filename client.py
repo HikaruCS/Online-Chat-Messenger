2a219b94293e910f3956c732db03bb8dfe04aa60
Morry <hm452317@gmail.com>
2025-03-12 17:40:32 +0900

    stage 1 without the relay system

--- client.py
+++ client.py
@@ -1,31 +1,48 @@
-# TODO: ChatGPTのアドバイスに従いながら、コードを書き進めていく
-
 import socket
+import threading
 
-# AF_INETを使用し、UDPソケットを作成
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = '192.168.0.38'
+server_address = '127.0.0.1'
 server_port = 9001
 
-address = ''
-port = 9050
-
-sock.bind((address, port))
-
-username = input('Enter your user name -> ')
-message = input(username + ": ")
-
-# ユーザー名とメッセージをバイト列に変換
+# 最初にユーザーネームを入力させる
+username = input('Enter your username >>> ').strip()  # 余計な空白文字を削除
 username_bytes = username.encode('utf-8')
-message_bytes = message.encode('utf-8')
+usernamelen = len(username_bytes).to_bytes(1, 'big')
 
-usernamelen = len(username_bytes)
-usernamelen_byte = usernamelen.to_bytes(1, 'big')
+def recieve_messages():
+    while True:
+        try:
+            data, server = sock.recvfrom(4096)
 
-packet = usernamelen_byte + username_bytes + message_bytes
+            usernamelen = data[0]
+            username_bytes = data[1:1 + usernamelen]
+            message_bytes = data[1 + usernamelen:]
 
-sock.sendto(packet, (server_address, server_port))
+            sender_username = username_bytes.decode('utf-8')
+            message = message_bytes.decode('utf-8')
 
+            print('\n[{}] {}'.format(sender_username, message))
+        except Exception as e:
+            print('Error receiving message: {}'.format(e))
+            break
 
-        
+# 受信用スレッドを開始
+receive_thread = threading.Thread(target=recieve_messages)
+receive_thread.start()
+
+# メインスレッドで送信処理
+try:
+    while True:
+        message = input()
+        if message.strip().lower() == 'exit':
+            break
+        message_bytes = message.encode('utf-8')
+        packet = usernamelen + username_bytes + message_bytes
+
+        sock.sendto(packet, (server_address, server_port))
+except KeyboardInterrupt:
+    print('\nClient shutting down...')
+
+sock.close()
