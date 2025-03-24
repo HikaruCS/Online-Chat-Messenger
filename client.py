d24c0778e16fe3f7f9baaed0627cd1ba3c1e9497
Morry <hm452317@gmail.com>
2025-03-24 22:16:49 +0900

    write a program for tcp networking

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+# TODO: UDPクライアントの実装
+
 import socket
 import threading
 
@@ -46,3 +48,89 @@ except KeyboardInterrupt:
     print('\nClient shutting down...')
 
 sock.close()
+
+class TCPClient:
+
+    # コンストラクタ
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.MAX_ROOM_NAME_BYTE = 2 ** 8
+        self.MAX_PAYLOAD_BYTE = 2 ** 29  # payloadはusernameのこと
+        self.MAX_TOKEN_BYTE = 255
+
+    # ヘッダ情報をフォーマットする関数
+    def protocol_header(room_name, operation, state, operation_payload):
+        room_name_size = len(room_name)
+        payload_size = len(operation_payload)
+        return room_name_size.to_bytes(1, 'big') + operation.to_bytes(1, 'big') + state.to_bytes(1, 'big') + payload_size.to_bytes(29, 'big')
+    
+    # ユーザー名入力
+    def input_username(self):
+        username = input('Enter your user name -> ')
+
+        if len(username) > self.MAX_PAYLOAD_BYTE:
+            print('User name should be not more than 2^29 bytes. Please try again.')
+            self.input_username()
+        elif len(username) == 0:
+            self.input_username()
+        else:
+            return username
+
+    # ルーム名を入力
+    def input_room_name(self, operation):
+        if operation == 1:
+            room_name = input('Enter new room name -> ')
+            if len(room_name) > self.MAX_ROOM_NAME_BYTE:
+                print('Rooom name should be not more than 2^8 bytes. Please try again.')
+                self.input_room_name()
+            elif len(room_name) == 0:
+                self.input_room_name()
+            else:
+                return room_name
+        elif operation == 2:
+            room_name = input('Enter a room name that you want to join -> ')
+            if len(room_name) > self.MAX_ROOM_NAME_BYTE:
+                print('Room name shoudlbe be not more than 2^8 bytes. Please try again.')
+                self.input_room_name()
+            elif len(room_name) == 0:
+                self.input_room_name()
+            else:
+                return room_name
+            
+   
+    # ここからtcp_main()
+    def tcp_main(self):
+        print('----- Online Chat Messenger -----')
+        print('1. Create a new chat room')
+        print('2. Join a chat room')
+        operation = ''
+        try:
+            operation = input('Enter your choice in a number -> ')
+        except:
+            self.tcp_main()
+
+        user_name = self.input_username()
+        room_name = self.input_room_name(operation)
+        state = 0
+
+        header = self.protocol_header(room_name, operation, state, user_name)
+        body = room_name.encode('utf-8') + user_name.encode('utf-8')
+        data = header + body
+
+        self.sock.send(data)
+        
+        self.sock.recv(4096)
+        self.sock.recv(4096)
+
+class UDPClient:
+    def __init__(self):
+        pass
+
+if __name__ == '__name__':
+    server_address = '127.0.0.1'
+    tcp_server_port = 9001
+    udp_server_port = 9002
+
+    tcp_client = TCPClient(server_address, server_port)
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-# TODO: ステージ2に取り掛かる
+# TODO: UDPサーバの実装
 
 import socket
 import time
+import secrets
 
 # socketを作成
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -54,3 +55,81 @@ while True:
         break
 
 sock.close()
+
+
+class TCPServer:
+
+    room_members_map = {}  # {room_name:[token, token, token.....]}
+    clients_map = {}  # {token:[client_address, room_name, palyload(username), isHost(True or False)]}
+
+    # コンストラクタ
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.server_address, self.server_port))
+        self.MAX_HEADER_SIZE = 32
+        self.MAX_TOKEN_SIZE = 255
+
+    def tcp_main(self):
+        self.sock.listen()
+
+        try:
+            while True:
+                connection, client_address = self.sock.accept()
+                print('connection from ', client_address)
+
+                data = connection.recev(4096)  # header + body
+
+                # header部分
+                header = connection.recv(self.MAX_HEADER_SIZE)
+                room_name_size = int.to_bytes(header[:1], 'big')
+                operation = int.to_bytes(header[1:2], 'big')
+                state = int.to_bytes(header[2:3], 'big')
+                operation_payload_size = int.to_bytes(header[3:], 'big')
+
+                # body部分
+                body = data[self.MAX_HEADER_SIZE:]
+                room_name = body[:room_name_size].decode('utf-8')
+                username = body[room_name_size:room_name_size + operation_payload_size].decode('utf-8')
+
+                print('room name: ', room_name)
+                print('user name: ', username)
+
+                token = secrets.token_bytes(self.MAX_TOKEN_SIZE)
+
+                # 新しいチャットルームを作成()
+                if operation == 1:
+                    isHost = True
+                    state = 1
+                    success_response = (room_name + ' is successfully created.').encode('utf-8')
+                    connection.send(success_response)
+                    token_response = ('Your token for this room is:').encode('utf-8')
+                    connection.send(token_response)
+                    connection.send(token)
+                    self.room_members_map[room_name] = [token]
+                    state = 2
+                    print('Create a new chat room (Host: {})'.format(username))
+                # 既存のルームに参加
+                elif operation == 2:
+                    isHost = False
+                    state = 1
+                    success_response = ('You can enter the room by the token below:')
+                    connection.send(success_response)
+                    print("This client's room: ", room_name)
+                    self.room_members_map[room_name].append(token)
+                    connection.send(token)
+                    state = 2
+                    print('Join the existed room')
+        except Exception as e:
+            print('Error: ' + str(e))
+        finally:
+            self.clients_map[token] = [client_address, room_name, username, isHost]
+
+    def tcp_start(self):
+        self.tcp_main()
+
+# ここからUDPサーバ
+class UDPServer:
+    def __init__(self):
+        pass
